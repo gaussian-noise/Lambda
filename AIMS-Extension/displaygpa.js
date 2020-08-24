f93bb450d721c0c154bf5553cce9b8a2fa94b71d
boredtea <es19btech11017@iith.ac.in>
2020-08-24 13:58:19 +0530

    added icon and tried to print student details

--- AIMS-Extension/displaygpa.js
+++ AIMS-Extension/displaygpa.js
@@ -1,30 +1,46 @@
-let data = JSON.parse(localStorage.getItem("courseGPA"));
+let studentData = JSON.parse(localStorage.getItem("student"));
+let courseData = JSON.parse(localStorage.getItem("courseGPA"));
 
 var completed_courses = document.getElementsByClassName("courses")[0];
 
+//sending student info to displaygpa.html
+document.getElementsByClassName("stuName")[0].innerText = studentData.Name;
+document.getElementsByClassName("stuRollno")[0].innerText = studentData.RollNo;
+document.getElementsByClassName("stuBranch")[0].innerText = studentData.Branch;
+document.getElementsByClassName("stuType")[0].innerText = studentData.StudentType;
+
 //inserting rows into the completed courses table
-var totalCredits = 0,
-    sumGrades = 0;
+var totalCredits = 0.00,
+    sumGrades = 0.00;
 
-for (i = 0; i < data.length; i++) {
+var excludedCourses = [
+    'Minor core',
+    'Honors core',
+    'Honours project',
+    'Honours coursework',
+    'FCC',
+    'Additional'
+];
 
-    if (data[i].NumberGrade != 'S') {
-        console.log(sumGrades);
-        console.log(totalCredits);
-        totalCredits = totalCredits + data[i].Credits;
-        sumGrades += (data[i].NumberGrade) * (data[i].Credits);
+for (i = 0; i < courseData.length; i++) {
+
+    //-1 is for the S and I grade
+    if (excludedCourses.indexOf(courseData[i].Type) != -1 && courseData[i].NumberGrade != -1) {
+        totalCredits = totalCredits + parseFloat(courseData[i].Credits);
+        sumGrades += parseFloat(courseData[i].NumberGrade) * parseFloat(courseData[i].Credits);
     }
 
     var row = document.createElement("tr");
-    row.innerHTML = `<td>${data[i].Code}</td>
-                    <td>${data[i].Course}</td>
-                    <td>${data[i].Type}</td>
-                    <td class="credit">${data[i].Credits}</td>
-                    <td class="grade">${data[i].Grade}</td>`
+    row.innerHTML = `<td>${courseData[i].Code}</td>
+                    <td>${courseData[i].Course}</td>
+                    <td>${courseData[i].Type}</td>
+                    <td class="credit">${courseData[i].Credits}</td>
+                    <td class="grade">${courseData[i].Grade}</td>`
 
     completed_courses.appendChild(row);
 }
 
 var CGPA = sumGrades / totalCredits;
+
 
 document.getElementsByClassName("cgpa")[0].innerText = CGPA;
